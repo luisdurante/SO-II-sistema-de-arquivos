6086e4220e96b20ddf9e2dacaa46b7107fe3fabe
Luis Durante <luisdurante2@outlook.com>
2020-11-25 21:54:50 -0300

    Update Main.java

--- src/sistemaDeArquivos/Main.java
+++ src/sistemaDeArquivos/Main.java
@@ -7,8 +7,9 @@ public class Main {
 		Header header = loadHeader();
 		header.readlines();
 		FilesStorage fileStorage = new FilesStorage();
-//		writeFile(fileStorage);
+//		writeFile(fileStorage, "HSH.sql");
 		readFile(fileStorage);
+//		deleteFile(fileStorage);
 		int option = 1;
 		
 		while(option != 0) {
@@ -20,7 +21,9 @@ public class Main {
 	
 	public static void printInterface() {
 		System.out.println("1- Listar Arquivos");
-		System.out.println("2- Guardar Arquivo");
+		System.out.println("2- Ler Arquivo");
+		System.out.println("3- Guardar Arquivo");
+		System.out.println("4- Deletar Arquivo");
 		System.out.println("0- Sair");
 	}
 	
@@ -28,18 +31,27 @@ public class Main {
 		return new Header();
 	}
 	
-	public static boolean writeFile(FilesStorage fileStorage) {
+	public static boolean writeFile(FilesStorage fileStorage, String fileName) {
 		fileStorage.getUpdatedByteArray();
-		int indexToWrite = fileStorage.checkIfFileFitsInBytesArray("teste.txt");
-		System.out.println("aaaaa = " +  indexToWrite);
-		fileStorage.writeFile("teste.txt", indexToWrite);
+		int indexToWrite = fileStorage.checkIfFileFitsInBytesArray(fileName);
+		int fileSize = fileStorage.getFileSize(fileName);
+		System.out.println("index = "+ indexToWrite + " size = " + fileSize);
+		fileStorage.writeFile(fileName, indexToWrite);
 		return true;
 	}
 	
 	public static boolean readFile(FilesStorage fileStorage) {
 		fileStorage.getUpdatedByteArray();
-		fileStorage.readFile(0,33);
+		fileStorage.readFile(2,30);
 		return true;
 	}
+	
+	public static boolean deleteFile(FilesStorage fileStorage) {
+		fileStorage.getUpdatedByteArray();
+		fileStorage.deleteFile(1,2);
+		return true;
+	}
+	
+	
 }
 
