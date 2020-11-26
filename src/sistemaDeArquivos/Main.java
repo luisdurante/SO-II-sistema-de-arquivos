fb7124b16f8dc32ef30ee34e0a8fe0485a4d671d
Luis Durante <luisdurante2@outlook.com>
2020-11-26 12:58:44 -0300

    Update Main.java

--- src/sistemaDeArquivos/Main.java
+++ src/sistemaDeArquivos/Main.java
@@ -1,54 +1,56 @@
 package sistemaDeArquivos;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Header {
-	private static final String FILENAME = "header.txt";
-	private PrintWriter pw;
-	private Scanner sc;
-	private File file;
-	private ArrayList<String> contentFile = new ArrayList<>();
-	
-	public Header() throws FileNotFoundException {
-//		String projectDirectory = System.getProperty("user.dir");
-//		String filePath = projectDirectory + "//src//sistemaDeArquivos//"+ FILENAME;
-		this.file = new File(FILENAME);
-		this.sc = new Scanner(this.file);
-		this.pw = new PrintWriter(this.file);
-	}
-	
-	public void readlines() {
-		while(this.sc.hasNextLine()) {
-			this.contentFile.add(this.sc.nextLine());
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Header header = loadHeader();
+		header.readlines();
+		FilesStorage fileStorage = new FilesStorage();
+//		writeFile(fileStorage, "HSH.sql");
+		readFile(fileStorage);
+//		deleteFile(fileStorage);
+		int option = 1;
+		
+		while(option != 0) {
+			printInterface();
+			option = scanner.nextInt();
 		}
+		scanner.close();
 	}
 	
-	public void showFiles() {
-		this.contentFile.forEach(file -> System.out.println(file));
+	public static void printInterface() {
+		System.out.println("1- Listar Arquivos");
+		System.out.println("2- Ler Arquivo");
+		System.out.println("3- Guardar Arquivo");
+		System.out.println("4- Deletar Arquivo");
+		System.out.println("0- Sair");
 	}
 	
-	public void addFile(String filename, int size, int position)throws IOException {
-		String file = filename + ";" + size + ";" + position;
-		this.contentFile.add(file);
+	public static Header loadHeader() {
+		return new Header();
 	}
 	
-	public void deleteFile(int position) {
-		this.contentFile.remove(position);
+	public static boolean writeFile(FilesStorage fileStorage, String fileName) {
+		fileStorage.getUpdatedByteArray();
+		int indexToWrite = fileStorage.checkIfFileFitsInBytesArray(fileName);
+		int fileSize = fileStorage.getFileSize(fileName);
+		System.out.println("index = "+ indexToWrite + " size = " + fileSize);
+		fileStorage.writeFile(fileName, indexToWrite);
+		return true;
 	}
 	
-	public void writeFile() {
-		for (String file : this.contentFile) {
-			this.pw.println(file);
-		}
-		this.close();
+	public static boolean readFile(FilesStorage fileStorage) {
+		fileStorage.getUpdatedByteArray();
+		fileStorage.readFile(2,30);
+		return true;
 	}
 	
-	private void close() {
-		sc.close();
-		pw.close();
+	public static boolean deleteFile(FilesStorage fileStorage) {
+		fileStorage.getUpdatedByteArray();
+		fileStorage.deleteFile(1,2);
+		return true;
 	}
+	
+	
 }
